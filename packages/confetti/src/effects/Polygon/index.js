851e5182fa81353c15f03de8dba3ad1299ded402
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-09-10 11:13:14 +0100

    .

--- packages/confetti/src/effects/Polygon/index.js
+++
@@ -1,9 +0,0 @@
-import { LeafletLayer } from '../Layer/index.js'
-
-export class LeafletPolygon extends LeafletLayer {
-  createInstance() {
-    const positions = this.props.positions || [];
-    return L.polygon(positions, this.props);
-  }
-}
-export const Polygon = (props, ...children) => new LeafletPolygon("polygon", props, ...children);
